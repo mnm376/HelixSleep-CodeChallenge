f4489137cd8b6e1d0981be570d302fb3524419ed
mnm376 <mnm376@nyu.edu>
2020-08-06 05:33:39 -0400

    frontend-nav complete

--- components/product/components/ProductSelectBox.vue
+++ components/product/components/ProductSelectBox.vue
@@ -1,17 +1,23 @@
 <template>
-    <div class="hs-product-select-box">
+    <div
+        :class="{
+                  'hs-product-select-box': true,
+                  'active': (this.$store.state.active_product.sku === product.sku)
+        }"
+    >
         <div
             v-on:click="update"
             class="hs-product-select-box_container"
         >
-          <img
-              :src="product.image.src"
-              :alt="product.image.alt"
-              class="hs-product-select-box_image"
-          />
+          <div class="hs-product-select-box_image-container">
+            <img
+                :src="product.image.src"
+                :alt="product.image.alt"
+                class="hs-product-select-box_image"
+            />
+          </div>
           <div class="hs-product-select-box_button">
             <div class="hs-product-select-box_button-text">{{ product.name }}</div>
-<!--            <div v-if="this.$store.state.active_product.sku===product.sku">Active</div>-->
           </div>
         </div>
     </div>
