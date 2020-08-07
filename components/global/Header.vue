88b9e8a9c30133a83aa170fd161c3e6a27611edc
mnm376 <mnm376@nyu.edu>
2020-08-06 20:22:15 -0400

    fix issue due to cache

--- components/global/Header.vue
+++ components/global/Header.vue
@@ -17,7 +17,9 @@
           <img src="/images/global/logo/logo.svg" alt="Allsleep Logo">
         </div>
         <div class="hs-header_nav-items">
-          <img src="/images/global/logo/logo.svg" alt="Allsleep Logo" class="hs-header_nav-logo">
+          <div class="hs-header_nav-logo">
+            <img src="/images/global/logo/logo.svg" alt="Allsleep Logo">
+          </div>
           <a href=# class="hs-header_nav-item active">{{ title }}</a>
           <a href=# class="hs-header_nav-item">Reviews</a>
           <a href=# class="hs-header_nav-item">About Us</a>
@@ -40,11 +42,6 @@
         data() {
           return {
             showMobileMenu: false
-          }
-        },
-        methods: {
-          openNav: function() {
-            document.getElementById("hs-header_nav").style.width = "100%";
           }
         }
   }
